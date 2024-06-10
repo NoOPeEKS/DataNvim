4116ea5f021a7029dbff7eeca101b2eb5bbaa888
NoOPeEKS <arnauapps@gmail.com>
2024-06-10 22:01:07 +0200

    Added autocompletion plugins

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,44 +1,53 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-          ensure_installed = { "lua_ls", "pyright" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({
-        filetypes = {"python", "quarto"}
-      })
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
-      vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, {})
-      vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, {})
-      vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, {})
-      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, {})
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {})
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {})
-      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, {})
-    end
-  },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				filetypes = { "python", "quarto" },
+			})
+
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, {})
+			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, {})
+			vim.keymap.set("n", "<space>wl", function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, {})
+			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, {})
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, {})
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, {})
+		end,
+	},
 }
