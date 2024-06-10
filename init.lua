1bc3ef89fea246ccf3d194ad3fbae06e5cffeed6
NoOPeEKS <arnauapps@gmail.com>
2024-06-10 20:15:28 +0200

    Install lazy and colorscheme

---
+++ init.lua
@@ -0,0 +1,22 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000},
+}
+local opts = {}
+require("lazy").setup(plugins, opts)
