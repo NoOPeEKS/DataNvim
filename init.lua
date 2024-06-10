cc0c08c58b7ab63933dd53551966d5a5f78d54dd
NoOPeEKS <arnauapps@gmail.com>
2024-06-10 20:37:36 +0200

    Add NvimTree plugin

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.opt.termguicolors = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -21,13 +22,28 @@ local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000},
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  }
 }
+
 local opts = {}
+
 require("lazy").setup(plugins, opts)
+
 require("catppuccin").setup()
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
@@ -35,4 +51,14 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<C-n>', ":NvimTreeToggle<CR>", {})
+vim.keymap.set('n', '<leader>e', ":NvimTreeFocus<CR>", {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "python"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
 vim.cmd.colorscheme "catppuccin"
