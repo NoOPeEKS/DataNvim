98093ed918a4394c6dd8285aac866df440204c23
NoOPeEKS <arnauapps@gmail.com>
2024-06-11 01:12:29 +0200

    Added keymaps for molten

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -4,11 +4,20 @@ return {
 	lazy = false,
 	build = ":UpdateRemotePlugins",
 	init = function()
-		-- this is an example, not a default. Please see the readme for more configuration options
 		vim.g.molten_image_provider = "image.nvim"
 		vim.g.molten_output_win_max_height = 12
 		vim.g.molten_virt_text_output = true
 		vim.g.molten_virt_lines_off_by_1 = true
 		vim.g.molten_virt_text_max_lines = 1
+    vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>",
+    { silent = true, desc = "Molten Init Kernel" })
+    vim.keymap.set("n", "<leader>ml", ":MoltenEvaluateLine<CR>",
+    { silent = true, desc = "Molten Evaluate Line" })
+    vim.keymap.set("v", "<leader>mv", ":<C-u>MoltenEvaluateVisual<CR>gv<ESC>",
+    { silent = true, desc = "Molten Evaluate Visual" })
+    vim.keymap.set("n", "<leader>mh", ":MoltenHideOutput<CR>",
+    { silent = true, desc = "Molten Hide Output" })
+    vim.keymap.set("n", "<leader>mo", ":noautocmd MoltenEnterOutput<CR>",
+    { silent = true, desc = "Molten Enter Output" })
 	end,
 }
