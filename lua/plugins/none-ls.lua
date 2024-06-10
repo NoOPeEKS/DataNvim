26bfeadcc68055d075164d14c3ca65815daf38ce
NoOPeEKS <arnauapps@gmail.com>
2024-06-10 21:40:16 +0200

    Added null-ls plugin and python formatters

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,14 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+      }
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+}
