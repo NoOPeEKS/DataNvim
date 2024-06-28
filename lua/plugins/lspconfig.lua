30c7c18afabb6e37e2250ef93c50dae50279e05e
NoOPeEKS <arnauapps@gmail.com>
2024-06-28 18:44:54 +0200

    feat: replace python linting with Ruff #8

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,66 +1,69 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ruff_lsp", "ruff", "pyright" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-        handlers = {
-          ["textDocument/publishDiagnostics"] = function () end,
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ruff_lsp.setup({
+        capabilities = capabilities,
+        init_options = {
+          settings = {
+            args = {},
+          }
+        }
+      })
+      lspconfig.pyright.setup({
+        settings = {
+          pyright = {
+            disableOrganizeImports = true, -- using Ruff
+          },
+          python = {
+            analysis = {
+              ignore = { '*' }, -- using Ruff
+            },
+          },
         },
-        on_attach = function(client, _)
-          client.server_capabilities.codeActionProvider = false
-        end,
-				filetypes = { "python"},
-				python = {
-					analysis = {
-						autoSearchPaths = true,
-						useLibraryCodeForTypes = true,
-            typeCheckingMode = "basic"
-					},
-				},
-			})
+      })
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
-			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, {})
-			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, {})
-			vim.keymap.set("n", "<space>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, {})
-			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, {})
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, {})
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, {})
-		end,
-	},
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+      vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, {})
+      vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, {})
+      vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, {})
+      vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, {})
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, {})
+    end,
+  },
 }
